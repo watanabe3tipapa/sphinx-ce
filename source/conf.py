62b8d9823444e821b8054a43936c97b08d84c89a
watanabe3tipapa <twpoet@nifty.com>
2023-09-28 11:11:38 +0900

    update

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,9 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,3 +28,8 @@ language = 'ja'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
